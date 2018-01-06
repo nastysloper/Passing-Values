c11dffa3f75959eae0668870302e73e353d4bb20
Rich Vogt <richvogtjr@gmail.com>
2018-01-05 19:47:03 -0600

    pass value to new screen

---
+++ app/src/main/java/com/nastysloper/sendmessage/MainActivity.java
@@ -0,0 +1,28 @@
+package com.nastysloper.sendmessage;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "com.nastysloper.sendmessage";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * called when user hits send
+     */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
